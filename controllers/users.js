642fafd05096445789d7159b6146e5bb107d0f21
Alexander Moiseyev <daklaktakpak@yandex.ru>
2020-10-23 20:44:17 +0500

    samesite with secure

--- controllers/users.js
+++ controllers/users.js
@@ -59,7 +59,8 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000,
         httpOnly: true,
-        sameSite: 'none, secure'
+        sameSite: "none",
+        secure: true,
       })
         .end();
     })
