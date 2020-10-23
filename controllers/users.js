83fc5597e436ac2e1667a7a4c1dc8a67000a754c
Alexander Moiseyev <daklaktakpak@yandex.ru>
2020-10-23 20:31:17 +0500

    samesite delete

--- controllers/users.js
+++ controllers/users.js
@@ -59,7 +59,6 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000,
         httpOnly: true,
-        sameSite: "None; Secure",
       })
         .end();
     })
